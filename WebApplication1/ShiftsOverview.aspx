9abaeb86829578473249729c727542640cf19ccf
bailer <jakob.sennerby@gmail.com>
2016-07-12 11:21:37 +0200

    view for Overview, like original

--- WebApplication1/ShiftsOverview.aspx
+++ WebApplication1/ShiftsOverview.aspx
@@ -1,4 +1,7 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ShiftsOverview.aspx.cs" Inherits="WebApplication1.ShiftsOverview" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ShiftsOverview.aspx.cs" Inherits="Schedule.ShiftsOverview" %>
+<asp:Content ID="ShiftOverview" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="Schedule" runat="server" AutoGenerateColumns="true">
+        <Columns>
+        </Columns>
+    </asp:GridView> />
 </asp:Content>
--- WebApplication1/ShiftsOverview.aspx.cs
+++ WebApplication1/ShiftsOverview.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Schedule.Models;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,11 +9,33 @@ using System.Web.UI.WebControls;
 
 namespace Schedule
 {
-    public partial class ShiftsOverview : System.Web.UI.Page
+    public partial class ShiftsOverview : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                PopulateSchedule();
+            }
 
+        }
+        public void PopulateSchedule()
+        {
+            SortedDictionary<string, List<ShiftWorker>> scheduleDict = new SortedDictionary<string, List<ShiftWorker>>();
+            var shiftworkers = ShiftWorker.getAll();
+            foreach (var s in shiftworkers)
+            {
+                if (scheduleDict.ContainsKey(s.date))
+                {
+                    scheduleDict[s.date].Add(s);
+                } else
+                {
+                    scheduleDict[s.date] = new List<ShiftWorker>();
+                    scheduleDict[s.date].Add(s);
+                }
+            }
+            Schedule.DataSource = scheduleDict;
+            Schedule.DataBind();
         }
     }
 }
