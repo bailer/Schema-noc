0155b40ec0f7dd23d94201f42e0b5b2ab341907f
joakim ualdern <joakim@jarvinen.se>
2016-10-06 15:11:54 +0200

    Revert "Revert "Revert "no work"""
    This reverts commit 0bc3a0d216a4a23977a23a38ae43d93969082357.

--- WebApplication1/WorkerList.aspx.designer.cs
+++ WebApplication1/WorkerList.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace Schedule {
         protected global::System.Web.UI.WebControls.Label deletedLabel;
         
         /// <summary>
-        /// deletedShiftsLabel control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label deletedShiftsLabel;
-        
-        /// <summary>
         /// workerDropdown control.
         /// </summary>
         /// <remarks>
@@ -94,15 +85,6 @@ namespace Schedule {
         protected global::System.Web.UI.WebControls.CheckBox Admin;
         
         /// <summary>
-        /// newButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button newButton;
-        
-        /// <summary>
         /// updateButton control.
         /// </summary>
         /// <remarks>
@@ -119,14 +101,5 @@ namespace Schedule {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button deleteButton;
-        
-        /// <summary>
-        /// deleteShiftsButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button deleteShiftsButton;
     }
 }
