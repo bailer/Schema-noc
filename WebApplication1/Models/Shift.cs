01e04b563bd3608ac24599d320a46e25c8b66270
joakim ualdern <joakim@jarvinen.se>
2016-10-06 15:11:48 +0200

    Revert "Funkar nu"
    This reverts commit 3e3769fcedf7daaa9c4b0550d8478befdcde8541.

--- WebApplication1/Models/Shift.cs
+++ WebApplication1/Models/Shift.cs
@@ -21,12 +21,6 @@ namespace Schedule.Models
             shift = db.shifts.Where(s => s.shiftId == id).FirstOrDefault<Shift>();
             return shift;
         }
-        public static Shift getShift(string id, WorkContext db)
-        {
-            Shift shift;
-            shift = db.shifts.Where(s => s.shiftId == id).FirstOrDefault<Shift>();
-            return shift;
-        }
         public static IEnumerable<Shift> getShifts(WorkContext db)
         {
             
